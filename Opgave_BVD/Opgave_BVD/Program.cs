8b2d91e25bae4b5eb8ddd5380b9ac99c25fd869b
emje-dania <emje7@student.eadania.dk>
2024-09-24 16:51:57 +0200

    the welcome intro

--- Opgave_BVD/Opgave_BVD/Program.cs
+++ Opgave_BVD/Opgave_BVD/Program.cs
@@ -10,6 +10,24 @@ namespace Opgave_BVD
     {
         static void Main(string[] args)
         {
+            //Start with into and ask for name
+            string name;
+
+            Console.WriteLine("Hello new player, please enter you name");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Welkome {name}, the rules are simple");
+            Console.WriteLine("type a number between 1 and 100 in order to guess the randomized number");
+            Console.WriteLine("the game will tell you how close you are");
+            Console.WriteLine("press enter when you are ready");
+
+            //the player picks a number
+            Console.WriteLine("type a number between 1 and 100");
+            int playerNumber = int.Parse(Console.ReadLine());
+
+
+
+            Console.ReadLine();
         }
     }
 }
