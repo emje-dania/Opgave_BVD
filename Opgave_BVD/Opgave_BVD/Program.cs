35172c58edf60a8ae7fd93d684a84c1b0e1c3f07
emje-dania <emje7@student.eadania.dk>
2024-09-24 17:50:54 +0200

    setup for repeat playing + too high/low
    have meda a while-loop in order to be able to keep playing the game.
    using if-statement in order to tell the game/player if they are to high/low from the target

--- Opgave_BVD/Opgave_BVD/Program.cs
+++ Opgave_BVD/Opgave_BVD/Program.cs
@@ -10,27 +10,52 @@ namespace Opgave_BVD
     {
         static void Main(string[] args)
         {
-            //Start with into and ask for name
+            // Varible list 
             string name;
+            bool restart = true;
+            int min = 1;
+            int max = 100;
+            int tries;
 
-            Console.WriteLine("Hello new player, please enter you name");
-            name = Console.ReadLine();
+            //create / in the game
+            while (restart)
+            {
+                //Start with into and ask for name
 
-            Console.WriteLine($"Welkome {name}, the rules are simple");
-            Console.WriteLine("type a number between 1 and 100 in order to guess the randomized number");
-            Console.WriteLine("the game will tell you how close you are");
-            Console.WriteLine("press enter when you are ready");
-            Console.ReadLine();
+                Console.WriteLine("Hello new player, please enter you name");
+                name = Console.ReadLine();
 
-            //the player picks a number
-            Console.WriteLine("type a number between 1 and 100");
-            int playerNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Welcome {name}, the rules are simple");
+                Console.WriteLine("type a number between 1 and 100 in order to guess the randomized number");
+                Console.WriteLine("the game will tell you how close you are");
+                Console.WriteLine("press enter when you are ready");
+                Console.ReadLine();
 
-            // Random number generator
-            Random randomNumber = new Random();
-            int rnd = randomNumber.Next(1, 101);
+                // Create a random number
+                Random randomNumber = new Random();
+                int rnd = randomNumber.Next(min, max + 1);
+
+                //the player picks a number
+                Console.WriteLine("type a number between 1 and 100");
+                int playerNumber = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Guess: " + playerNumber);
+
+                //Use an if statement to choose what happens when the guess is too high or low
+                if (playerNumber > rnd)
+                {
+                    Console.WriteLine(playerNumber + " is too high");
+                } 
+                else if (playerNumber < rnd)
+                {
+                    Console.WriteLine(playerNumber + " is too low");
+                }
+            }
             
-            Console.WriteLine("the random number is:  "+rnd);
+
+            
+
+
 
 
             Console.ReadLine();
